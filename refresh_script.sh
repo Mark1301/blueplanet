72c7f0d89f64747ec3fdedf89a72cfad5a5db94d
utsavvadhar@blueplanet <utsavvadhar2017@gmail.com>
2021-06-19 16:16:52 +0530

    Update refresh_script.sh

--- refresh_script.sh
+++ refresh_script.sh
@@ -6,12 +6,10 @@ timestamp() {
 
 while true
 do
-        sudo php jsonToDB.php
+
         sudo php jsonToDBFinance.php
-        sudo php jsonToWixFinance.php
-        sudo php jsonToDBGujarat.php
-        sudo php jsonToDBSaurashtra.php
-        
+
+
         echo " - News Added - Current time: "
         timestamp
         sleep 600
